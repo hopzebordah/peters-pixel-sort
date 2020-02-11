c501dd55f689cd806d4656f8a3ca1e57163a4839
Peters <alc98@live.missouristate.edu>
2020-02-11 12:42:29 -0600

    add webapp structure

---
+++ pps/src/main/java/com/alcocerap/pps/PpsApplication.java
@@ -0,0 +1,13 @@
+package com.alcocerap.pps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpsApplication.class, args);
+	}
+
+}
---
+++ pps/src/test/java/com/alcocerap/pps/PpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alcocerap.pps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
