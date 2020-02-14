cb79946f96effc730cb26bbc561cfbf0cd20b22b
Alex Peters <aalcocerpeters@gmail.com>
2020-02-14 09:48:11 -0600

    working PoC for serving SPA from spr.boot

---
+++ pps-server/src/main/java/com/alcocerap/pps/controller/PageNotFoundController.java
@@ -0,0 +1,33 @@
+package com.alcocerap.pps.controller;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.ResourceUtils;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+@ControllerAdvice
+public class PageNotFoundController {
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<String> renderDefaultPage() {
+        try {
+            File indexFile = ResourceUtils.getFile("classpath:public/index.html");
+            FileInputStream inputStream = new FileInputStream(indexFile);
+            String body = StreamUtils.copyToString(inputStream, Charset.defaultCharset());
+            return ResponseEntity.ok().contentType(MediaType.TEXT_HTML).body(body);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("There was an error completing the action.");
+        }
+    }
+    
+  }
--- pps-server/src/main/java/com/alcocerap/pps/controller/PpsController.java
+++ pps-server/src/main/java/com/alcocerap/pps/controller/PpsController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/api")
 public class PpsController {
 
     @GetMapping("hello")
